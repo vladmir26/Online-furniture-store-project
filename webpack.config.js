3281ec59bb52f396629bcb91058ca22f32ec64e5
vlad_mir <mironow.slava2012@yandex.ru>
2022-11-16 18:07:16 +0300

    Добавил файл gitignore, добавил картинки в папку dist, добавил файл reset через npm, продолжил верстку страницы

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
        path: path.resolve(__dirname, 'dist'),
        clean: true,
        filename: '[name].[contenthash].js',
+       assetModuleFilename: 'assets/[name][ext]'
 
     },
     plugins: [
@@ -45,6 +46,35 @@ module.exports = {
                "css-loader",
                "sass-loader"
             ],
+            },
+            {
+              test: /\.(jpe?g|png|svg)$/i,
+              use: [
+                {
+                  loader: 'image-webpack-loader',
+                  options: {
+                    mozjpeg: {
+                      progressive: true,
+                    },
+                    // optipng.enabled: false will disable optipng
+                    optipng: {
+                      enabled: false,
+                    },
+                    pngquant: {
+                      quality: [0.65, 0.90],
+                      speed: 4
+                    },
+                    gifsicle: {
+                      interlaced: false,
+                    },
+                    // the webp option will enable WEBP
+                    webp: {
+                      quality: 75
+                    }
+                  }
+                }
+              ],
+              type: 'asset/resource',
             }
         ]
     }
