e47da67dae835b50141388280c7d78991db8695e
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-01-13 15:32:28 +0300

    Сделал сборку gulp, gподключил pug и scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,70 @@
 const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+const browserSync = require('browser-sync').create();
 
-gulp.task('default', defaultTask);
-
-function defaultTask(done) {
-    done();
+const paths = {
+    root: './dist',
+    templates: {
+        pages: './src/*.pug',
+        src: './src/*.pug',
+        dest: './dist/',
+    },
+    styles: {
+        main: './src/*.scss',
+        src: './src/*.scss',
+        dest: './dist/'
+    },
+    sripts: {
+        src: './srs/*js',
+        dest: './dist/'
+    }
 }
+
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.templates.src, templates)
+}
+
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
+function templates() {
+    return gulp.src(paths.templates.pages)
+       .pipe(pug({ pretty: true }))
+       .pipe(gulp.dest(paths.root))
+}
+
+
+
+function styles() {
+    return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
+function scripts() {
+   return gulp.src(paths.sripts.src)
+     .pipe(gulpWebpack(webpackConfig, webpack))
+     .pipe(gulp.dest(paths.sripts.dest));
+}
+
+exports.templates = templates;
+exports.styles = styles;
+exports.scripts = scripts;
+
+gulp.task('default', gulp.series(
+    gulp.parallel(styles, templates),
+    gulp.parallel(watch, server)
+));
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,21 @@
-const path = require('path');
+const webpack = require('webpack');
 const fs = require('fs.promises');
-const mode = process.env.NODE_ENV || 'development'; 
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-const devMode = mode === 'development';
 
-const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
+const config = {
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    filename: "[name].index.js"
+  },
+  plugins: [
+      new UglifyJSPlugin({
+        sourceMap: true
+      })
+    ]
+  };
 
-module.exports = {
-    mode,
-    target,
-    devtool,
-    devServer: {
-        port: 3000,
-        open: true,
-    },
-    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
-    output: {
-       path: path.resolve(__dirname, 'dist'),
-       clean: true,
-       filename: '[name].[contenthash].js',
-       assetModuleFilename: 'assets/[name][ext]'
-
-    },
+  module.exports = config;
    
-            {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env']
-                }
-              }
-            }
-           
-
-        ]
-    }
-}
